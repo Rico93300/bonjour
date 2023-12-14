ef9af660bf939cba8f206957568bd39d46689a81
rico <e.muller3@yahoo.fr>
2023-12-14 16:52:31 +0100

    14/12

---
+++ js/bonjour.js
@@ -0,0 +1,22 @@
+// let prenom = prompt("entrez votre prenom ");
+// alert("bonjour " + prenom);
+// let nom = prompt("entrez votre nom ");
+//  alert("bonjour " + prenom + " " + nom);
+
+// var a = Number(prompt("entrer votre prix ht"));
+// var b = (20 * a) / 100;
+// console.log(b);
+// var c = a + b;
+// console.log(a * b);
+//  document.write("le prix ttc es de " + c);
+
+
+// let d = Number(prompt("quel est la temperature en celsius?"));
+// let e = d * (9 / 5) + 32;
+// document.write(" en fahrenheit ca donne " + e);
+
+let nombre1 = 5;
+let nombre2 =3;
+[nombre1, nombre2] = [nombre2, nombre1];
+console.log(nombre1);
+console.log(nombre2);
